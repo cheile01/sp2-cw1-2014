2f1239c77009e84b9288878b2f7d0cf6148bf9fd
cheile01 <cheile01@dcs.bbk.ac.uk>
2014-10-04 12:56:21 +0100

    ArrayInput function added.

--- src/main.java
+++ src/main.java
@@ -11,20 +11,18 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //TODO: code application logic here
-        Scanner in = new Scanner(System.in);
-        int input = 1;
-        int[] first = new int[100];
-        int i = 0;
-        do{
-            System.out.print("whats your number? ");
-            input = in.nextInt();
-            if(!inArray(input, first)){
-               first[i++] = input; 
-            } 
-        }while(input != 0);
+
+        int[] first = getIntArray("Enter data for array 1 (0 to finish): ");
+        int[] second = getIntArray("Enter data for array 2 (0 to finish): ");
+        
+        System.out.println();
+        System.out.println("Values for array 1 is: "+delimit(first," "));
+        System.out.println("Values for array 2 is: "+delimit(second," "));
     }
 
+    
+    
+    
     /**
      * @param needle(int) - value searched for
      * @param haystack[int] array of int to be searched
@@ -32,12 +30,14 @@ public class main {
      */
     public static boolean inArray(int needle, int[] haystack ){
         boolean eval = false;
-        
         for (int straw : haystack) {
-            //shorten processing time.
+            //shorten processing time as all values are set to 0 in array.
             if(straw==0){
                 break;
-            }  
+            }
+            if(needle==straw){
+                eval = true;
+            }
         }  
         return eval;
     }
@@ -50,11 +50,32 @@ public class main {
     public static String delimit(int[] values, String delimiter ){
         String result = "";
         for (int value : values) {
+            if(value==0){
+                break;
+            }
             result += value+delimiter;
         }  
         return result;
     }
     
+    /**
+     * @param message(String) Message to output
+     * @return array of int
+     */
+    public static int[] getIntArray(String message ){
+        Scanner in = new Scanner(System.in);
+        int input = 1;
+        int[] array = new int[100];
+        int i = 0;
+        do{
+            System.out.print(message);
+            input = in.nextInt();
+            if(!inArray(input, array)){
+               array[i++] = input; 
+            } 
+        }while(input != 0);
+        return array;
+    }
     
     
     
