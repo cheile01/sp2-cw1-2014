a0fc1c33b2d8d9102aa27b7d99f59a01e8490c82
cheile01 <cheile01@dcs.bbk.ac.uk>
2014-10-04 12:04:34 +0100

    First Loop

---
+++ src/main.java
@@ -0,0 +1,36 @@
+/**
+ * Coursework One
+ * @author christian Heiler (cheile01)
+ * @version 0.1.0
+ */
+
+import java.util.Scanner;
+
+public class main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //TODO: code application logic here
+        Scanner in = new Scanner(System.in);
+        int i;
+        while(i != 0){
+            System.out.print("whats your number? ");
+            i = in.nextInt();
+        }
+        
+    }
+
+    /**
+     * @param int the command line arguments
+     */
+    public static boolean inArray(int needle, int[] haystack ){
+        boolean eval = false;
+        
+        for (int straw : haystack) {
+            System.out.println(straw);
+        }
+        
+        return eval;
+    }
+}
